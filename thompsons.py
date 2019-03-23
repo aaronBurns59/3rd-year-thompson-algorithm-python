93e8101ee168d3523d578250a1c803dff35b2409
aaronBurns59 <aaronburns59@gmail.com>
2019-03-23 16:15:12 +0000

    created an nfa using the two states and pushed it onto an nfa stack

--- thompsons.py
+++ thompsons.py
@@ -15,7 +15,7 @@ class NFA:
 
     #Constructor for the NFA class
     #self references the current instance of the class
-    #self has to be called first and is not needed to class an instance of the class
+    #self has to be called first and is not needed to create an instance of the class
     def _init_(self, initial, final):
         #self.initial/final is the class var after = is the local var
         self.initial=initial
@@ -24,10 +24,18 @@ class NFA:
 #populate using the result of the shunting yard algroithm
 def compile(postfix):
     nfaStack = []
+    #for creating an nfa fragment for a non special character
     for c in postfix:
         #creating a new instance of the state class
         accept = state()
         initial= state()
+        #the state is called whatever character is read in
         initial.label = c
-
+        #connects the initial state to the accept state
+        initial.edge1 = accept
+        #edge2 is not needed for the initial state because its the first state
+        #create a new NFA object and puts it on the nfa stack
+        #the initial state above and the accept state are used by the 
+        #nfa constructor to create an nfa for a non special character
+        nfaStack.append(NFA(initial, accept))
 print('WORKS')
