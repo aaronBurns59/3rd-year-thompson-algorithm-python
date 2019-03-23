2d00d885c9fc95fa73d91aaf75ca1192cf8b154a
aaronBurns59 <aaronburns59@gmail.com>
2019-03-23 16:06:48 +0000

    Created a constructor for the nfa class and a function that reads in postfix RE which also instantiates two state class objects

--- thompsons.py
+++ thompsons.py
@@ -10,10 +10,24 @@ class state:
 
 #When declaring an instance of a class
 class NFA: 
-    initialState = None
-    finalState = None
+    initial = None
+    final = None
 
+    #Constructor for the NFA class
+    #self references the current instance of the class
+    #self has to be called first and is not needed to class an instance of the class
+    def _init_(self, initial, final):
+        #self.initial/final is the class var after = is the local var
+        self.initial=initial
+        self.final=final
 
-
+#populate using the result of the shunting yard algroithm
+def compile(postfix):
+    nfaStack = []
+    for c in postfix:
+        #creating a new instance of the state class
+        accept = state()
+        initial= state()
+        initial.label = c
 
 print('WORKS')
