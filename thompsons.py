51695b69b6664b8813d4f13e04301d0c87a6367e
aaronBurns59 <aaronburns59@gmail.com>
2019-03-23 15:53:34 +0000

    Created classes to represent states and nfa's

--- thompsons.py
+++ thompsons.py
@@ -1,2 +1,19 @@
 #Thompson's Construction
 #Aaron Burns
+
+#No modifiers for classes in python
+class state:
+    #Value None means you don't want to set a value yet
+    label = None
+    edge1 = None
+    edge2 = None
+
+#When declaring an instance of a class
+class NFA: 
+    initialState = None
+    finalState = None
+
+
+
+
+print('WORKS')
